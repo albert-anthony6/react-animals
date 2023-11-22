5ab5dc02aca7677ed3dc7d7ef1b8bab74181ea33
Albert Valdes <albert.anthony1999@gmail.com>
2023-11-22 06:27:26 -0600

    chore: initialized project

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    devSourcemap: true,
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use 'sass:math';
+          @use 'sass:color';
+          @use 'sass:map';
+          @import './src/assets/scss/_variables.scss';
+          @import './src/assets/scss/_mixins.scss';
+          @import './src/assets/scss/_functions.scss';
+          @import './src/assets/scss/_animations.scss';
+        `,
+      },
+    },
+  },
+})
