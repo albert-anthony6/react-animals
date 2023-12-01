63592d57c0ab017b948cda3f50aad26cabf7ad2b
Albert Valdes <albert.anthony1999@gmail.com>
2023-11-30 19:45:56 -0600

    style: updated modal menu styles

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -11,86 +11,88 @@ type Props = {
 
 function AppHeader({ activeModal, setActiveModal }: Props) {
   return (
-    <nav className="app-header">
+    <header className="app-header">
       <Link to="/" className="logo">
         <ZooLogo onClick={() => setActiveModal(false)} />
       </Link>
-      <ul className="ul-child">
-        <li>
-          <Link to="#">Membership</Link>
-        </li>
-        <li>
-          <Link to="#">Tickets</Link>
-          <ul className="ul-child">
-            <li>Admission</li>
-            <li className="submenu">
-              Special Events
-              <ul className="ul-child--nested">
-                <li>Zoo Lights</li>
-                <li>Breakfast With Santa</li>
-              </ul>
-            </li>
-            <li>Host an Event</li>
-            <li>Birthday Parties</li>
-            <li>Close Encounters</li>
-          </ul>
-        </li>
-        <li>
-          <Link to="#">Conservation</Link>
-          <ul className="ul-child">
-            <li>Our Research Team</li>
-            <li>Conservation Action Network</li>
-            <li className="submenu">
-              Arboretum
-              <ul className="ul-child--nested">
-                <li>Shade Garden</li>
-              </ul>
-            </li>
-            <li>Go Green</li>
-            <li>Memphis Aazk</li>
-            <li>Master Garden Corner</li>
-            <li>Conservation Six</li>
-          </ul>
-        </li>
-        <li>
-          <Link to="#">Wildlife</Link>
-          <ul className="ul-child">
-            <li>Explore Exhibits</li>
-            <li>Adopt an Animal</li>
-            <li>Zoo News</li>
-            <li>Live Animal Care</li>
-          </ul>
-        </li>
-        <li>
-          <Link to="#">Education</Link>
-          <ul className="ul-child">
-            <li>Field Trips</li>
-            <li>Twilight Tours</li>
-            <li>Virtual Zoo Programs</li>
-            <li>Zoo Labs</li>
-            <li>Owl Programs</li>
-            <li>Zoo Snooze</li>
-            <li>Zoo Strolls</li>
-            <li>Zoo Camps</li>
-            <li>Zoo Scounts Programs</li>
-          </ul>
-        </li>
-        <li>
-          <Link to="#">Donate</Link>
-          <ul className="ul-child">
-            <li>Eco Express Train</li>
-            <li>Give Now</li>
-            <li>Volunteer</li>
-          </ul>
-        </li>
-        <li onClick={() => setActiveModal(!activeModal)}>
-          <div className={activeModal ? "modal-button active-modal" : "modal-button"}>
-            Plan Your Adventure
-            <FontAwesomeIcon icon={faChevronDown} />
-          </div>
-        </li>
-      </ul>
-    </nav>
+      <nav>
+        <ul className="ul-child">
+          <li>
+            <Link to="#">Membership</Link>
+          </li>
+          <li>
+            <Link to="#">Tickets</Link>
+            <ul className="ul-child">
+              <li>Admission</li>
+              <li className="submenu">
+                Special Events
+                <ul className="ul-child--nested">
+                  <li>Zoo Lights</li>
+                  <li>Breakfast With Santa</li>
+                </ul>
+              </li>
+              <li>Host an Event</li>
+              <li>Birthday Parties</li>
+              <li>Close Encounters</li>
+            </ul>
+          </li>
+          <li>
+            <Link to="#">Conservation</Link>
+            <ul className="ul-child">
+              <li>Our Research Team</li>
+              <li>Conservation Action Network</li>
+              <li className="submenu">
+                Arboretum
+                <ul className="ul-child--nested">
+                  <li>Shade Garden</li>
+                </ul>
+              </li>
+              <li>Go Green</li>
+              <li>Memphis Aazk</li>
+              <li>Master Garden Corner</li>
+              <li>Conservation Six</li>
+            </ul>
+          </li>
+          <li>
+            <Link to="#">Wildlife</Link>
+            <ul className="ul-child">
+              <li>Explore Exhibits</li>
+              <li>Adopt an Animal</li>
+              <li>Zoo News</li>
+              <li>Live Animal Care</li>
+            </ul>
+          </li>
+          <li>
+            <Link to="#">Education</Link>
+            <ul className="ul-child">
+              <li>Field Trips</li>
+              <li>Twilight Tours</li>
+              <li>Virtual Zoo Programs</li>
+              <li>Zoo Labs</li>
+              <li>Owl Programs</li>
+              <li>Zoo Snooze</li>
+              <li>Zoo Strolls</li>
+              <li>Zoo Camps</li>
+              <li>Zoo Scounts Programs</li>
+            </ul>
+          </li>
+          <li>
+            <Link to="#">Donate</Link>
+            <ul className="ul-child">
+              <li>Eco Express Train</li>
+              <li>Give Now</li>
+              <li>Volunteer</li>
+            </ul>
+          </li>
+          <li onClick={() => setActiveModal(!activeModal)}>
+            <div className={activeModal ? "modal-button active-modal" : "modal-button"}>
+              Plan Your Adventure
+              <FontAwesomeIcon icon={faChevronDown} />
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
