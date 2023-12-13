6fcd190f606312cd19f9955fbbedf13aed74cc9b
Albert Valdes <albert.anthony1999@gmail.com>
2023-12-13 13:40:52 -0600

    wip: added screen change

--- src/components/MenuModal.tsx
+++ src/components/MenuModal.tsx
@@ -1,26 +1,38 @@
 import './MenuModal.scss';
-import { useMediaQuery } from 'react-responsive';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRightLong, faClock, faCalendar, faLocationDot, faSquareParking, faCircleQuestion, faUtensils } from "@fortawesome/free-solid-svg-icons";
 import Collapsible from 'react-collapsible';
+import useScreenSize from '../utilities/useScreenSize';
 
 function MenuModal() {
-    const isMobile = useMediaQuery({ query: `(max-width: 1080px)` });
+    const screenSize = useScreenSize();
     
     return (
         <div className="menu-modal">
             <Collapsible
+                transitionTime={screenSize.width >= 1080 ? 0.1 : 400}
+                transitionCloseTime={1}
                 trigger="Plan Your Adventure"
                 triggerOpenedClassName="overall-collapsible"
-                triggerClassName="overall-collapsible"
+                triggerClassName="overall-collapsible__closed"
                 contentInnerClassName="overall-collapsible--content"
                 triggerDisabled={false}
-                open={true}
+                open={screenSize.width >= 1080 ? true : false}
             >
                 <section className="special-events">
                     <h4 className="green-text">Special Adventures</h4>
                     {
-                        isMobile ? (
+                        screenSize.width >= 1080 ? (
+                            <div>
+                                <img src="src/assets/images/zoo_lights.jpg" alt="Zoo Lights." />
+                                <h4>Zoo Lights</h4>
+                                <h5>Select Nights November 24-January 6 | 5:30-9:30pm</h5>
+                                <p>
+                                    WHAT A BRIGHT TIME, IT'S THE RIGHT TIME... Get ready to immerse yourself in the enchanting world of the holiday season as Truist Zoo Lights returns to Memphis Zoo. With mesmerizing light displays, beloved classics, and exciting new additions, this is the place to be for a merry and bright holiday experience. The iconic Light Tunnel will once again brighten up the night with vibrant colors, while Magic Mr. Nick will dazzle guests with exciting performances. Add even MORE magic with the Gingerbread Forest, the beautiful Chinese lanterns, Alice's Tea Party, AND MUCH MORE. If you're in the mood for some ice skating, lace up your skates and take a spin on the rink. It's the perfect opportunity to showcase your skills. And who can resist the delectable goodness of s'mores? Make your own gooey treats and relish the sweet taste of the season. It pairs perfectly with a warm cup of hot chocolate. It's not truly the holiday season without a visit to Santa Claus. Have your chance to meet him and share your holiday wishes. Who knows, you might end up on the nice list! Whether it's a romantic date night, a family outing, or a fun evening with friends, Zoo Lights offers the perfect setting.
+                                    <a href="https://www.memphiszoo.org/zoo-lights" className="learn-more"> Learn More <FontAwesomeIcon icon={faArrowRightLong} /></a>
+                                </p>
+                            </div>
+                        ) : (
                             <Collapsible
                                 trigger="Zoo Lights"
                                 triggerOpenedClassName="events-collapsible__opened"
@@ -39,16 +51,6 @@ function MenuModal() {
                                     </p>
                                 </div>
                             </Collapsible>
-                        ) : (
-                            <div>
-                                <img src="src/assets/images/zoo_lights.jpg" alt="Zoo Lights." />
-                                <h4>Zoo Lights</h4>
-                                <h5>Select Nights November 24-January 6 | 5:30-9:30pm</h5>
-                                <p>
-                                    WHAT A BRIGHT TIME, IT'S THE RIGHT TIME... Get ready to immerse yourself in the enchanting world of the holiday season as Truist Zoo Lights returns to Memphis Zoo. With mesmerizing light displays, beloved classics, and exciting new additions, this is the place to be for a merry and bright holiday experience. The iconic Light Tunnel will once again brighten up the night with vibrant colors, while Magic Mr. Nick will dazzle guests with exciting performances. Add even MORE magic with the Gingerbread Forest, the beautiful Chinese lanterns, Alice's Tea Party, AND MUCH MORE. If you're in the mood for some ice skating, lace up your skates and take a spin on the rink. It's the perfect opportunity to showcase your skills. And who can resist the delectable goodness of s'mores? Make your own gooey treats and relish the sweet taste of the season. It pairs perfectly with a warm cup of hot chocolate. It's not truly the holiday season without a visit to Santa Claus. Have your chance to meet him and share your holiday wishes. Who knows, you might end up on the nice list! Whether it's a romantic date night, a family outing, or a fun evening with friends, Zoo Lights offers the perfect setting.
-                                    <a href="https://www.memphiszoo.org/zoo-lights" className="learn-more"> Learn More <FontAwesomeIcon icon={faArrowRightLong} /></a>
-                                </p>
-                            </div>
                         )
                     }
                     <h3>Book Your Adventure</h3>
@@ -178,6 +180,52 @@ function MenuModal() {
                     </div>
                 </section>
             </Collapsible>
+            <nav className="mobile-nav">
+                <ul>
+                    <li className="category-link"><a href="#">Membership</a></li>
+
+                    <li className="category-link"><a href="#">Tickets</a></li>
+                    <li className="general-link"><a href="#">Admission</a></li>
+                    <li className="general-link"><a href="#">Special Events</a></li>
+                    <li className="specific-link"><a href="#">Zoo Lights</a></li>
+                    <li className="specific-link"><a href="#">Breakfast With Santa</a></li>
+                    <li className="general-link"><a href="#">Host An Event</a></li>
+                    <li className="general-link"><a href="#">Birthday Parties</a></li>
+                    <li className="general-link"><a href="#">Close Encounters</a></li>
+
+                    <li className="category-link"><a href="#">Conservation</a></li>
+                    <li className="general-link"><a href="#">Our Research Team</a></li>
+                    <li className="general-link"><a href="#">Conservation Action Network</a></li>
+                    <li className="general-link"><a href="#">Arboretum</a></li>
+                    <li className="specific-link"><a href="#">Shade Garden</a></li>
+                    <li className="general-link"><a href="#">Go Green</a></li>
+                    <li className="general-link"><a href="#">Memphis Aazk</a></li>
+                    <li className="general-link"><a href="#">Master Gardener Corner</a></li>
+                    <li className="general-link"><a href="#">Conservation Six</a></li>
+
+                    <li className="category-link"><a href="#">Wildlife</a></li>
+                    <li className="general-link"><a href="#">Explore Exhibits</a></li>
+                    <li className="general-link"><a href="#">Adopt An Animal</a></li>
+                    <li className="general-link"><a href="#">Zoo News</a></li>
+                    <li className="general-link"><a href="#">Live Animals Cams</a></li>
+
+                    <li className="category-link"><a href="#">Education</a></li>
+                    <li className="general-link"><a href="#">Field Trips</a></li>
+                    <li className="general-link"><a href="#">Twilight Tours</a></li>
+                    <li className="general-link"><a href="#">Virtual Zoo Programs</a></li>
+                    <li className="general-link"><a href="#">Zoo Labs</a></li>
+                    <li className="general-link"><a href="#">Owl Programs</a></li>
+                    <li className="general-link"><a href="#">Zoo Snooze</a></li>
+                    <li className="general-link"><a href="#">Zoo Strolls</a></li>
+                    <li className="general-link"><a href="#">Zoo Camps</a></li>
+                    <li className="general-link"><a href="#">Zoo Scouts Programs</a></li>
+
+                    <li className="category-link"><a href="#">Donate</a></li>
+                    <li className="general-link"><a href="#">Eco Express Train</a></li>
+                    <li className="general-link"><a href="#">Give Now</a></li>
+                    <li className="general-link"><a href="#">Volunteer</a></li>
+                </ul>
+            </nav>
         </div>
     )
 }
