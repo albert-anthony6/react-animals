3a206997af2a181769657b35463e579c1992d74f
Albert Valdes <albert.anthony1999@gmail.com>
2023-12-14 15:38:50 -0600

    feat: added mobile responsive carousel

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -2,9 +2,25 @@ import './HomeView.scss';
 import videoSrc from '../assets/videos/hero_bg.mp4';
 import IconScrollIndicator from '../assets/icons/icon_scroll_indicator.svg?react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowDownLong } from "@fortawesome/free-solid-svg-icons";
-import Carousel from 'react-multi-carousel';
+import { faArrowDownLong, faArrowRightLong, faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+import Carousel, { ButtonGroupProps, StateCallBack }  from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+
+const CustomButtonGroup = ({ next, previous, goToSlide, carouselState }: ButtonGroupProps) => {
+    const { totalItems, slidesToShow } = carouselState as StateCallBack;
+
+    return (
+      <div className="custom-button-group">
+        <button onClick={() => [goToSlide?.(totalItems - slidesToShow), previous?.()]}>
+            <FontAwesomeIcon icon={faArrowLeftLong} />
+        </button>
+        <button onClick={() => [goToSlide?.(0), next?.()]}>
+            <FontAwesomeIcon icon={faArrowRightLong} />
+        </button>
+      </div>
+    );
+};
+  
 
 function HomeView() {
     const slides = [
@@ -93,31 +109,47 @@ function HomeView() {
       };
 
   return (
-    <section className="home-view">
-        <div className="hero-banner">
-            <div className="hero-text">
-                <h4>Explore a Wonderful World of</h4>
-                <h1>Wildlife</h1>
-            </div>
-            <div className="bg-video">
-                <video loop autoPlay muted>
-                    <source src={ videoSrc } type="video/mp4" />
-                    Your browser does not support the video tag. It is recommended to upgrade your browser.
-                </video>
-            </div>
-        </div>
-        <div className="scroll-indicator">
-            <IconScrollIndicator />
-            <FontAwesomeIcon icon={faArrowDownLong} />
-        </div>
-        <Carousel responsive={responsive} infinite={true} draggable={false}>
-            {slides.map((slide) => (
-                <div className="slide">
-                    <img key={slide.img} src={`./src/assets/images/carousel-images/${slide.img}`} />
+    <main className="home-view">
+        <section className="hero-section">
+            <div className="hero-banner">
+                <div className="hero-text">
+                    <h4>Explore a Wonderful World of</h4>
+                    <h1>Wildlife</h1>
                 </div>
-            ))}
-        </Carousel>
-    </section>
+                <div className="bg-video">
+                    <video loop autoPlay muted>
+                        <source src={ videoSrc } type="video/mp4" />
+                        Your browser does not support the video tag. It is recommended to upgrade your browser.
+                    </video>
+                </div>
+            </div>
+            <div className="scroll-indicator">
+                <IconScrollIndicator />
+                <FontAwesomeIcon icon={faArrowDownLong} />
+            </div>
+        </section>
+        <section className="experience-section">
+            <div className="experience-section--text">
+                <h4 className="green-text">Experience The Wild</h4>
+                <h2>Your Passport To Wildlife From Across The Globe</h2>
+                <p className="headline">Memphis Zoo is home to 3,500+ animals. Find your favorites and discover new species to love!</p>
+            </div>
+            <Carousel
+                responsive={responsive}
+                className="carousel"
+                infinite={false}
+                draggable={false}
+                arrows={false}
+                customButtonGroup={<CustomButtonGroup />}
+            >
+                {slides.map((slide, idx) => (
+                    <div className="slide" key={`${idx}-${slide.img}`}>
+                        <img src={`./src/assets/images/carousel-images/${slide.img}`} />
+                    </div>
+                ))}
+            </Carousel>
+        </section>
+    </main>
   )
 }
 
